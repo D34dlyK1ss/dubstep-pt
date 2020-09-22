e3f12b4ab8a7a07380e148e3567c1b23d8a0acd6
D34dlyK1ss <rags.ftjs@gmail.com>
2020-09-23 00:29:42 +0100

    Updated

--- js/button.js
+++ js/button.js
@@ -1,18 +1,21 @@
-$('body').prepend('<a href="body" class="back-to-top page-scroll">Voltar para cima</a>');
+(function($) {
+  "use strict"; 
+    
+  $('body').prepend('<a href="body" class="back-to-top page-scroll">Voltar para cima</a>');
+    
+  let amountScrolled = 600;
+    
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > amountScrolled) {
+       $('a.back-to-top').fadeIn('500');
+    }
+    else {
+       $('a.back-to-top').fadeOut('500');
+    }
+  });
 
-let amountScrolled = 600;
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > amountScrolled) {
-    $('a.back-to-top').fadeIn('500');
-  }
-  else {
-    $('a.back-to-top').fadeOut('500');
-  }
-});
-
-
-/* Removes Long Focus On Buttons */
-$(".button, a, button").mouseup(function() {
-	$(this).blur();
-});
+  /* Removes Long Focus On Buttons */
+  $(".button, a, button").mouseup(function() {
+     $(this).blur();
+  });
+})(jQuery);
